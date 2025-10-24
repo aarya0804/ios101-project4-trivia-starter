a6ad42306bb944eae664f20580fd367153328672
aarya0804 <ava6560@psu.edu>
2025-10-23 23:53:08 -0400

    final#1

---
+++ Trivia/TriviaModels.swift
@@ -0,0 +1,8 @@
+//
+//  TriviaModels.swift
+//  Trivia
+//
+//  Created by Aarya Awasthy on 10/23/25.
+//
+
+import Foundation
---
+++ Trivia/TriviaQuestionService.swift
@@ -0,0 +1,8 @@
+//
+//  TriviaQuestionService.swift
+//  Trivia
+//
+//  Created by Aarya Awasthy on 10/23/25.
+//
+
+import Foundation
